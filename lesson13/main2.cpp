8b74091765d6ff77d9229c1be84d8b29f1811bf3
Michael <m.tsekhmistro@gmail.com>
2020-01-26 10:43:16 +0000

    description readme  On branch master  Your branch is up to date with 'cpp/master'.  Changes to be committed: 	new file:   README.md 	new file:   lesson13/func.cpp 	new file:   lesson13/main2.cpp 	new file:   lesson13/my.cpp 	new file:   lesson14/first.cpp

---
+++ lesson13/main2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    for(int index = 0 ;index < 10 ;index++){
+        cout <<index <<endl;
+
+    }
+    return 0;
+}
---
+++ lesson13/my.cpp
@@ -0,0 +1,15 @@
+#include  <iostream>
+using namespace std;
+void print();
+char getSymbo();
+int main(){
+    char a = getSymbol()
+    char arr[] = {'h','e','l','l'};
+    int arrInt = {1,2,3,4};
+    return 0;
+}
+
+char  getSymbol(){
+    char symbol = '0';
+    return symbol;
+}
